3ebb18c62d696488f297f4354dc5fbd570b2e649
GeheimCoding <geheim.coding@gmail.com>
2023-12-26 12:19:40 +0100

    Update dependencies + fresh start

--- runtime_api/runtime_api_format/patches/floating_point.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug, Deserialize)]
-#[serde(untagged)]
-pub enum FloatingPoint<T> {
-    SpecialValue(String),
-    Value(T),
-}
-
-type Float = FloatingPoint<f32>;
-type Double = FloatingPoint<f64>;
--- runtime_api/runtime_api_format/patches/maybe_cycle.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug, Deserialize)]
-#[serde(untagged)]
-pub enum MaybeCycle<T> {
-    Cycle { cycle_id: u32 },
-    Value(Box<T>),
-}
